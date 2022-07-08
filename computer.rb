da438aeef2ba7a7288b7e6654fef80886e4c5db9
humanathome <eabicic@gmail.com>
2022-07-08 14:45:27 +0200

    make basic working version of the game where player can pick their role

--- computer.rb
+++ computer.rb
@@ -2,11 +2,9 @@
 
 # computer player class
 class ComputerPlayer
-  attr_reader :secret_code
-
   def initialize; end
 
-  def generate_secret_code
-    @secret_code = Game::PEG_COLORS.sample(4)
+  def generate_color_code
+    Game::PEG_COLORS.sample(4)
   end
 end
--- game.rb
+++ game.rb
@@ -7,7 +7,7 @@ require_relative 'input'
 
 # main game class
 class Game
-  attr_accessor :white_pegs, :black_pegs
+  attr_accessor :white_pegs, :black_pegs, :secret_code
 
   include Display
   include Input
@@ -22,10 +22,10 @@ class Game
   def set_role
     role = prompt_for_role
     if role == 1
-      puts 'Your role: code-maker'
+      puts "Your role: code-maker\n\n"
       @player.role = :code_maker
     else
-      puts 'Your role: code-breaker'
+      puts "Your role: code-breaker\n\n"
       @player.role = :code_breaker
     end
   end
@@ -33,7 +33,11 @@ class Game
   def setup_game
     display_intro_and_rules
     set_role
-    @computer.generate_secret_code
+    @secret_code = if @player.role == :code_maker
+                     prompt_for_color_code
+                   else
+                     @computer.generate_color_code
+                   end
   end
 
   def reset_pegs
@@ -42,6 +46,7 @@ class Game
   end
 
   def check_guess(guess, code)
+    reset_pegs
     guess.each_with_index do |peg, index|
       if code[index] == peg
         @black_pegs += 1
@@ -59,19 +64,45 @@ class Game
     turn + 1 == 12
   end
 
-  def play
-    setup_game
+  # call this method if the player is the code-breaker
+  def human_code_breaker
     12.times do |turn|
-      reset_pegs
       puts "\nTurn #{turn + 1} of 12"
-      guess = prompt_for_guess
-      check_guess(guess, @computer.secret_code)
-      display_pegs(@black_pegs, @white_pegs)
+      guess = prompt_for_color_code
+      check_guess(guess, @secret_code)
       if won?(@black_pegs)
-        display_winning_message
+        display_winning_message('You')
         break
       end
+      display_pegs(@black_pegs, @white_pegs)
+      display_losing_message('You') if lost?(turn)
     end
-    display_code(@computer.secret_code)
+  end
+
+  # call this method if the player is the code-maker
+  def computer_code_breaker
+    12.times do |turn|
+      puts "\nTurn #{turn + 1} of 12"
+      display_thinking_message
+      computer_guess = @computer.generate_color_code
+      display_computer_guess(computer_guess)
+      check_guess(computer_guess, @secret_code)
+      if won?(@black_pegs)
+        display_winning_message('Computer')
+        break
+      end
+      display_pegs(@black_pegs, @white_pegs)
+      display_losing_message('Computer') if lost?(turn)
+    end
+  end
+
+  def play
+    setup_game
+    if @player.role == :code_breaker
+      human_code_breaker
+    else
+      computer_code_breaker
+    end
+    display_code(@secret_code)
   end
 end
