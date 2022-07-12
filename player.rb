fcc67bbff8ca453b7184851a754275f34546cb6f
humanathome <eabicic@gmail.com>
2022-07-12 14:44:47 +0200

    add player_turn method

--- player.rb
+++ player.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
+require_relative 'input'
+
 # human player class
 class HumanPlayer
   attr_accessor :role
 
+  include Input
+
   def initialize; end
+
+  def player_turn
+    prompt_for_color_code
+  end
 end
